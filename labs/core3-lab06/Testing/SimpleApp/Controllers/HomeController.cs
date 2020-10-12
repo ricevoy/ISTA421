4b8ca78e6b1338ebd5474af4765a3317bacd1ceb
Rawlston Morton <68116799+ricevoy@users.noreply.github.com>
2020-10-11 21:05:28 -0500

    My first commit

---
+++ labs/core3-lab06/Testing/SimpleApp/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SimpleApp.Models;
+namespace SimpleApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        //what kind of method is this? action method!
+        // expects a view with the name of the action method
+        // located inside views/home
+        public ViewResult Index()
+        {
+            return View(Product.GetProducts());
+        }
+    }
+}
---
+++ quizzes/Quiz 23/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace Quiz_23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is C# Quiz 23");
+            Console.WriteLine("Enter the highest number to find primes: ");
+            string input = Console.ReadLine();
+            long findTo = long.Parse(input);
+            List<long> primes = new List<long>();
+
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            Parallel.For(0, findTo + 1, x =>
+            {
+                bool foundP = isPrime(x);
+                if (foundP)
+                {
+                    primes.Add(x);
+
+                }
+            });
+            sw.Stop();
+            TimeSpan ts = sw.Elapsed;
+
+            //output
+            Console.WriteLine("PRINTING PRIMES");
+            foreach (int element in primes)
+                Console.Write($" {element} ");
+            Console.WriteLine($"\n\nfound primes in time {ts}");
+        }
+
+        private static bool isPrime(long n)
+        {
+            bool isP = true;
+            for (int i = 2; i <= (int)Math.Sqrt(n); i++)
+                if (n % i == 0)
+                {
+                    isP = false;
+                    break;
+                }
+            return isP;
+        }
+    }
+}
+
